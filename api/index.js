ffa6badda662ab2895502b8485d593d81a9ce637
Vinit N K <vinitnk2004@gmail.com>
2026-07-22 23:16:21 +0530

    feat: set up express server and route API requests to app

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+const express = require('express');
 const app = require('../backend/src/app');
 
-module.exports = app;
+const server = express();
+server.use('/api', app);
+
+module.exports = server;
